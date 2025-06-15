79f8f1da493dcb8b33f34f6565d930d1604a37ed
YICHUNLIN <yichun03@gmail.com>
2025-06-15 11:27:10 +0800

    build

--- web/src/App.js
+++ web/src/App.js
@@ -15,6 +15,7 @@ import Concrete from './components/concrete';
 import AsphaltConcrete from './components/asphaltConcrete';
 import DesignMap from './components/designMap';
 import ConcreteInverse from './components/concreteInverse';
+import QCTest from './components/qctest';
 
 const pages = [
   {name: '座標轉換', to: '/km/gis', route: <Kmgis/>},
@@ -24,6 +25,7 @@ const pages = [
   {name: '瀝青混凝土配比設計', to: '/ac/design', route: <AsphaltConcrete/>},
   {name: '咖啡廳位置配置', to: '/map/design', route: <DesignMap/>},
   {name: '混凝土反向設計', to: '/concrete/inverse', route: <ConcreteInverse/>},
+  {name: '品管考試', to: '/qc', route: <QCTest/>},
 ];
 
 const App = ({}) => {
